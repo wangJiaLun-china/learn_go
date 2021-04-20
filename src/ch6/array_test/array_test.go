3e3c98d301fb055580ddcc2ea352ebc680443631
allen <961105114@qq.com>
2021-04-20 11:20:00 +0800

    数组遍历

--- src/ch6/array_test/array_test.go
+++ src/ch6/array_test/array_test.go
@@ -1,1 +1,26 @@
-package ch6
+package array_test
+
+import "testing"
+
+func TestArrayInit(t *testing.T)  {
+	var arr[3]int
+	arr1 := [4] int{1,2,3,4}
+	arr2 := [...] int{1,2,3,4}
+	arr1[1] = 7
+	t.Log(arr[1], arr[2])
+	t.Log(arr1[1], arr1[2])
+	t.Log(arr2[1], arr2[2])
+}
+
+func TestArrayTravel(t *testing.T)  {
+	arr2 := [...] int{2,3,3,4}
+	for i := 0; i < len(arr2); i++ {
+		t.Log(arr2[i])
+	}
+	for i := range arr2 {
+		t.Log(i)
+	}
+	for index, item := range arr2 {
+		t.Log(index,item)
+	}
+}
