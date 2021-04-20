b351ecfba5859bca12e7a8f89ee1e282eab4a794
allen <961105114@qq.com>
2021-04-20 10:51:11 +0800

    数组比较，以及位运算符

---
+++ src/ch4/operator_test/operator_test.go
@@ -0,0 +1,27 @@
+package operator_test
+
+import "testing"
+
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+
+
+func TestCompareArray(t *testing.T)  {
+	a:=[...]int{1,2,3,4}
+	b:=[...]int{1,3,3,4}
+	// 长度不同数组比较会直接编译错误
+	//c:=[...]int{1,2,3,4,5}
+	d:=[...]int{1,2,3,4}
+	t.Log(a==b,  a==d)
+}
+
+func TestBitClear(t *testing.T)  {
+	a:= 7 // 0111
+	a = a &^ Readable
+	a = a &^ Writable
+	t.Log(a&Readable==Readable, a&Writable==Writable, a&Executable==Executable)
+}
