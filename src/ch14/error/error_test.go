9c184ee53b509fd29e1dbea9bebe05fe3b5b3f61
allen <961105114@qq.com>
2021-04-25 17:20:33 +0800

    错误处理

---
+++ src/ch14/error/error_test.go
@@ -0,0 +1,35 @@
+package error
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+var LessThanTwoError = errors.New("n should be not less than 2")
+var LargerThenHundredError = errors.New("n should be not larger than 100")
+
+func GetFibonacci(n int) ([]int, error) {
+	if n < 2 {
+		return nil, LessThanTwoError
+	}
+	if n > 100 {
+		return nil, LargerThenHundredError
+	}
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList, nil
+}
+
+func TestGetFibonacci(t *testing.T) {
+	if v, err := GetFibonacci(1); err != nil {
+		if err == LessThanTwoError {
+			fmt.Println("it is less.")
+		}
+		t.Error(err)
+	} else {
+		t.Log(v)
+	}
+}
