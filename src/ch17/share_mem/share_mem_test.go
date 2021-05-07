3681000f0b52dcaaca3e6a76d38f4cbad12e518d
allen <961105114@qq.com>
2021-05-07 15:38:42 +0800

    WaitGroup使用

--- src/ch17/share_mem/share_mem_test.go
+++ src/ch17/share_mem/share_mem_test.go
@@ -32,3 +32,22 @@ func TestCounterThreadSafe(t *testing.T) {
 	time.Sleep(time.Second * 1)
 	t.Logf("counter = %d", counter)
 }
+
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
