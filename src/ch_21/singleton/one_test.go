6a762004a5f68e4debd2158b6c3bb7cd0acb1a66
allen <961105114@qq.com>
2021-05-10 18:12:13 +0800

    并发下的单例

---
+++ src/ch_21/singleton/one_test.go
@@ -0,0 +1,34 @@
+package singleton
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+type Singleton struct {
+}
+
+var singleInstance *Singleton
+var once sync.Once
+
+func GetSingletonObj() *Singleton {
+	once.Do(func() {
+		fmt.Println("Create Obj")
+		singleInstance = new(Singleton)
+	})
+	return singleInstance
+}
+
+func TestGetSingletonObj(t *testing.T) {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			obj := GetSingletonObj()
+			fmt.Printf("%x", obj)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
