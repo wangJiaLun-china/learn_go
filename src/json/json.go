9690dadc88927595bdd11ca9e3f1cefceefae87a
wangjialun-china <wangjialun.go@gmail.com>
2021-06-04 13:54:04 +0800

    json 在结构体中的配置

--- src/json/json.go
+++ src/json/json.go
@@ -7,18 +7,21 @@ import (
 
 /**
 小写字母开头字段会忽略，不参与json编解码
+后面配置 `json:"-"` 忽略
+配置 `json:"Age_int"` 变成json中别名
+配置 `json:"Age_int,string"` 变成json中别名,并转换类型
+配置 `json:"Age_int,omitempty"` 变成json中别名,如果为空就不编解码
 */
 type User struct {
-	Id   int
-	Age  int
+	Id   int `json:"-"`
+	Age  int `json:"Age_int,string"`
 	Name string
 }
 
 func main() {
 	zhangsan := User{
-		1,
-		20,
-		"zhangsan",
+		Id:   1,
+		Name: "zhangsan",
 	}
 	// func Marshal(v interface{}) ([]byte, error) {
 	encodeInfo, err := json.Marshal(&zhangsan)
